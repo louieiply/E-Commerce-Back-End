de5950089f4787976f45886d0a3c4ecd1e6783f0
louieiply <louieiply@gmail.com>
2022-06-14 18:04:13 +0930

    Implement CRUD API endpoints

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # E-Commerce-Back-End
 E-Commerce Back End is the largest sector of the electronics industry, generating an estimated $29 trillion in 2019. E-commerce platforms like Shopify and WooCommerce provide a suite of services to businesses of all sizes. 
+
+
+## Video
+https://youtu.be/yIhFbJHetSQ
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,19 +4,55 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findAll({
+      include: [{model: Category},{  model:Tag, through: ProductTag, as: "tags"}],
+    });
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findByPk(req.params.id , {
+      include: [{model: Category},{  model:Tag, through: ProductTag, as: "tags"}],
+    });
+
+    if(!productData){
+      res.status(404).json({ message: 'No Product found with that id!' });
+      return;      
+    }
+
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // create new product
 router.post('/', (req, res) => {
+  // try {
+  //   if (!req.body.product_name){
+  //     res.status(400).json({ message: "Tag name is missing." });
+  //   }
+  //   else{
+  //     const tagData = await Tag.create({
+  //       tag_name: req.body.tag_name,
+  //     })
+  //     res.status(200).json(tagData);
+  //   }
+
+  // } catch (err) {
+  //   res.status(500).json(err);
+  // }
   /* req.body should look like this...
     {
       product_name: "Basketball",
@@ -89,8 +125,26 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const productData = await Tag.findByPk(req.params.id);
+    
+    if(!productData){
+      res.status(400).json({message: "Product ID not found."});
+      return;
+    }
+
+    const deletedProductData = await Product.destroy({
+      where: {
+        id:req.params.id,
+      },
+    });
+    
+    res.status(200).json({message: "Product has been deleted."});
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
